bc6f5a57a4149762deebe95d44b745c583a7b271
mike_oliver <mikewilliamoliver@gmail.com>
2013-12-10 10:36:30 -0500

    Added label toggling when user has insufficient funds.

---
+++ Assets/SocialPlay-SDK/Store/Script/PurchaseButtonDisaply.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PurchaseButtonDisaply : MonoBehaviour
+{
+    public UIButton ActiveButton;
+    public UILabel InactiveLabel;
+    public string ReplacmentText = "";
+
+
+    public void SetInactive()
+    {
+        if (!string.IsNullOrEmpty(ReplacmentText) && ReplacmentText != InactiveLabel.text)
+        {
+            InactiveLabel.text = ReplacmentText;
+        }
+        ActiveButton.gameObject.SetActive(false);
+        InactiveLabel.gameObject.SetActive(true);
+    }
+
+    public void SetActive()
+    {
+        if (!string.IsNullOrEmpty(ReplacmentText) && ReplacmentText != InactiveLabel.text)
+        {
+            InactiveLabel.text = ReplacmentText;
+        }
+        ActiveButton.gameObject.SetActive(true);
+        InactiveLabel.gameObject.SetActive(false);
+    }
+
+    public void SetState(bool state)
+    {
+        if (state)
+        {
+            SetActive();
+        }
+        else
+        {
+            SetInactive();
+        }
+    }
+}
