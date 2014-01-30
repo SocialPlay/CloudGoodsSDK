350f7fbec74f70c8f25391c51864830aba083bec
Lionel Sy <lionely.sy@gmail.com>
2014-01-30 13:35:08 -0500

    added item texture cache to item image loading for optimization

--- Assets/SocialPlay-SDK/Scripts/Item/NGUIContainerGameItem.cs
+++ Assets/SocialPlay-SDK/Scripts/Item/NGUIContainerGameItem.cs
@@ -50,9 +50,30 @@ public class NGUIContainerGameItem : MonoBehaviour
 
     public void GetItemTexture(string URL)
     {
+        if (ItemTextureCache.ItemTextures != null)
+            GetItemTextureFromCache(URL);
+        else
+            GetItemTextureFromWeb(URL);
+    }
+
+    private void GetItemTextureFromWeb(string URL)
+    {
         WWW www = new WWW(URL);
 
         StartCoroutine(OnReceivedItemTexture(www));
+    }
+
+    void GetItemTextureFromCache(string imgURL)
+    {
+        if (ItemTextureCache.ItemTextures.ContainsKey(imgURL))
+        {
+            UITexture uiTexture = gameObject.GetComponentInChildren<UITexture>();
+            uiTexture.material = new Material(uiTexture.material.shader);
+            uiTexture.mainTexture = ItemTextureCache.ItemTextures[imgURL];
+            uiTexture.transform.localPosition -= Vector3.forward * 2;
+        }
+        else
+            GetItemTextureFromWeb(imgURL);
     }
 
     IEnumerator OnReceivedItemTexture(WWW www)
---
+++ Assets/SocialPlay-SDK/Scripts/Setup/ItemTextureCache.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemTextureCache : MonoBehaviour {
+
+    public static Dictionary<string, Texture2D> ItemTextures;
+}
