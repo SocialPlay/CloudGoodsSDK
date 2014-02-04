841300c075979a8cc47bb416218fb1a054b954db
Lionel Sy <lionely.sy@gmail.com>
2014-02-04 12:43:18 -0500

    added new unity test plugin, now have unit test running for item texture caching

--- Assets/SocialPlay-SDK/Scripts/Setup/ItemTextureCache.cs
+++ Assets/SocialPlay-SDK/Scripts/Setup/ItemTextureCache.cs
@@ -1,8 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class ItemTextureCache : MonoBehaviour {
 
-    public static Dictionary<string, Texture2D> ItemTextures;
+    public Dictionary<string, Texture2D> ItemTextures = new Dictionary<string,Texture2D>() ;
+
+    public void GetItemTexture(string URL, Action<ImageStatus, Texture2D> callback)
+    {
+        try
+        {
+            if (ItemTextures.ContainsKey(URL))
+            {
+                callback(ImageStatus.Cache, ItemTextures[URL]);
+            }
+            else
+                GetItemTextureFromWeb(URL, callback);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+            callback(ImageStatus.Error, null);
+        }
+    }
+
+    private void GetItemTextureFromWeb(string URL, Action<ImageStatus, Texture2D> callback)
+    {
+        WWW www = new WWW(URL);
+
+        StartCoroutine(OnReceivedItemTexture(www, callback, URL));
+    }
+
+    IEnumerator OnReceivedItemTexture(WWW www, Action<ImageStatus, Texture2D> callback, string imageURL)
+    {
+        yield return www;
+
+        ItemTextures.Add(imageURL, www.texture);
+        callback(ImageStatus.Web, www.texture);
+    }
+
+    public enum ImageStatus
+    {
+        Web,
+        Cache,
+        Error
+    }
 }
---
+++ Assets/UnityTestTools/SocialplaySDKUnitTests/HasReceivedItemTextureTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HasReceivedItemTextureTest : MonoBehaviour {
+
+    public ItemTextureCache itemTextureCache;
+
+    void Start()
+    {
+        itemTextureCache.ItemTextures.Clear();
+        itemTextureCache.GetItemTexture("http://www.desicomments.com/dc3/01/209982/209982.gif", OnReceivedItemTexture);
+    }
+
+    void OnReceivedItemTexture(ItemTextureCache.ImageStatus statusMsg, Texture2D texture)
+    {
+        if (statusMsg == ItemTextureCache.ImageStatus.Web)
+            IntegrationTest.Pass(gameObject);
+        else
+            IntegrationTest.Fail(gameObject);
+    }
+}
