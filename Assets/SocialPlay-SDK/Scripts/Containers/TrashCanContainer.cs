7a8469b492d61c4defb9760268fba903388847c5
mike_oliver <mikewilliamoliver@gmail.com>
2014-02-25 12:17:42 -0500

    Added Trash can to SDK

---
+++ Assets/SocialPlay-SDK/Scripts/Containers/TrashCanContainer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashCanContainer : ItemContainer
+{
+    protected override ContainerAddState MyContainerAddState(ItemData modified)
+    {
+        return new ContainerAddState(ContainerAddState.ActionState.Add);
+    }
+
+    protected override void AddItem(ItemData modified, int amount = -1, bool isSave = true)
+    {
+        modified.ownerContainer.Remove(modified, false, modified.stackSize);
+        Destroy(modified.gameObject);
+    }
+
+    protected override void RemoveItem(ItemData modified, bool isMovingToAnotherContainer, int amount = -1)
+    {
+        //Nothing to remove.
+    }
+
+    public override int Contains(ItemData modified)
+    {
+        return 0;
+    }
+
+    public override void Clear()
+    {
+        ///Nothing to clear.
+    }
+}
---
+++ Assets/SocialPlay-SDK/Scripts/Setup/PlainTextTooltip.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlainTextTooltip : MonoBehaviour, ITooltipSetup
+{
+    public string tooltipText;
+
+    public string Setup()
+    {
+        return tooltipText;
+    }
+}
