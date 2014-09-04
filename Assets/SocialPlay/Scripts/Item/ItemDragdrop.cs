084519db57082218d473dcffeb6a7885bc8797fc
Lionel Sy <lionely.sy@gmail.com>
2014-09-04 14:39:49 -0400

    Added new drag drop component for the new Unity 4.6 UI

---
+++ Assets/SocialPlay/Scripts/Item/ItemDragdrop.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ItemDragdrop : MonoBehaviour {
+
+    Transform canvasObject;
+    Transform lastParent;
+
+    Vector3 lastPosition;
+
+    bool isDragging = false;
+
+    void Start()
+    {
+        canvasObject = GameObject.Find("Canvas").transform;
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragging == false)
+        {
+            isDragging = true;
+            lastPosition = transform.localPosition;
+            lastParent = transform.parent;
+        }
+
+        Vector3 newPosition = Input.mousePosition;
+
+        newPosition -= new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        transform.parent = canvasObject;
+
+        transform.localPosition = newPosition;
+    }
+
+    void OnMouseUp()
+    {
+        if (isDragging)
+        {
+            transform.parent = lastParent;
+            transform.localPosition = lastPosition;
+            Debug.Log("Item Dropped");
+            isDragging = false;
+        }
+    }
+}
