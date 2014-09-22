eeae9bddccc5197fbce2b1a8017c8b524dc36f38
mike_oliver <mikewilliamoliver@gmail.com>
2014-09-22 12:57:46 -0400

    Fixed: tool tip to use static even so any UI can easily hook into the tool tip System

--- Assets/SocialPlay/Scripts/UI/TooltipHandler.cs
+++ Assets/SocialPlay/Scripts/UI/TooltipHandler.cs
@@ -1,61 +1,25 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
+using System;
 
 
-public class TooltipHandler : MonoBehaviour
-{
-    private static TooltipHandler handler = null;
-    public bool isFollowMouse;
-    public Text tooltipText = null;
-    public Image background = null;
-    private bool isShowing = false;
-    private Vector2 originalDelta = Vector2.zero;
+public class TooltipHandler {
 
-    void Awake()
-    {
-        if (tooltipText == null)
-            tooltipText = this.GetComponentInChildren<Text>();
-        if (background != null)
-        {
-            originalDelta = background.rectTransform.sizeDelta;
-        }
-        if (handler == null)
-        {
-            handler = this;
-        }
-        Hide();
-    }
+    public static event Action<bool, string> ChangeTooltip;    
 
     public static void Show(string tooltip)
-    {
-        if (handler == null) return;
-        handler.gameObject.SetActive(true);
-        handler.tooltipText.text = tooltip;
-        handler.isShowing = true;
+    {       
+        if (ChangeTooltip != null)
+        {
+            ChangeTooltip(true, tooltip);
+        }
     }
 
     public static void Hide()
     {
-        if (handler == null) return;
-        handler.gameObject.SetActive(false);
-        handler.isShowing = false;
-    }
-
-    void Update()
-    {
-        if (handler == this && handler.isShowing)
+        if (ChangeTooltip != null)
         {
-            if (isFollowMouse)
-            {
-                Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.transform.localPosition.z);
-                newPosition -= new Vector3(Screen.width / 2, Screen.height / 2, 0);
-                this.transform.localPosition = newPosition;
-            }
-            if (background != null)
-            {
-                background.rectTransform.sizeDelta = tooltipText.rectTransform.sizeDelta + originalDelta;
-            }
+            ChangeTooltip(false, null);
         }
-    }
+    } 
 }
---
+++ Assets/SocialPlay/Scripts/UI/UnityUITooltipHandler.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UnityUITooltipHandler : MonoBehaviour
+{
+    public Text tooltipText = null;
+    public Image background = null;
+    private bool isShowing = false;
+    private Vector2 originalDelta = Vector2.zero;
+    public bool isFollowMouse;
+
+    void Awake()
+    {
+        if (tooltipText == null)
+            tooltipText = this.GetComponentInChildren<Text>();
+        if (background != null)
+        {
+            originalDelta = background.rectTransform.sizeDelta;
+        }
+        TooltipHandler.ChangeTooltip += TooltipHandler_ChangeTooltip;
+        TooltipHandler_ChangeTooltip(false, null);  
+    }
+
+    void TooltipHandler_ChangeTooltip(bool show, string tooltip)
+    {     
+        if (show)
+        {
+            gameObject.SetActive(true);
+            tooltipText.text = tooltip;
+            isShowing = true;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            isShowing = false;
+        }
+    }
+
+    void Update()
+    {
+        if (!isShowing) return;
+        if (isFollowMouse)
+        {
+            Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.transform.localPosition.z);
+            newPosition -= new Vector3(Screen.width / 2, Screen.height / 2, 0);
+            this.transform.localPosition = newPosition;
+        }
+        if (background != null)
+        {
+            background.rectTransform.sizeDelta = tooltipText.rectTransform.sizeDelta + originalDelta;
+        }
+    }
+}
