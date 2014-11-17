d8aee63cf073a1231f8d5a4e22fd0b859e74f92c
Lionel Sy <lionely.sy@gmail.com>
2014-11-17 15:01:38 -0500

    Added new fail cases for editor premium currency Purchase

--- Assets/CloudGoods/Scripts/Store/PurchaseBundles/PlatformPurchasing/EditorPremiumCurrencyPurchaser.cs
+++ Assets/CloudGoods/Scripts/Store/PurchaseBundles/PlatformPurchasing/EditorPremiumCurrencyPurchaser.cs
@@ -8,15 +8,49 @@ public class EditorPremiumCurrencyPurchaser : MonoBehaviour, IPlatformPurchaser 
     public event Action<string> RecievedPurchaseResponse;
     public event Action<string> OnPurchaseErrorEvent;
 
+    public enum EditorPurchaseResultType
+    {
+        Pass,
+        Fail,
+        Error
+    }
+
+    public EditorPurchaseResultType resultType = EditorPurchaseResultType.Pass;
+
     public void Purchase(PremiumBundle bundleItem, int amount, string userID)
     {
-        OnReceivedPurchaseResponse("Successful Purchase");
+        switch (resultType)
+        {
+            case EditorPurchaseResultType.Pass:
+                OnReceivedPurchaseResponse("Successful Purchase");
+                break;
+            case EditorPurchaseResultType.Fail:
+                OnReceivedPurchaseResponse("Failed Purchase");
+                break;
+            case EditorPurchaseResultType.Error:
+                OnReceivedPurchaseResponse("Error Occured when purchasing");
+                break;
+        }
     }
 
     public void OnReceivedPurchaseResponse(string data)
     {
-        if(RecievedPurchaseResponse != null)
-            RecievedPurchaseResponse(data);
+        switch (data)
+        {
+            case "Successful Purchase":
+                if (RecievedPurchaseResponse != null)
+                    OnReceivedPurchaseResponse(data);
+                break;
+            case "Failed Purchase":
+                if (OnPurchaseErrorEvent != null)
+                    OnPurchaseErrorEvent(data);
+                break;
+            case "Error Occured when purchasing":
+                if (OnPurchaseErrorEvent != null)
+                    OnPurchaseErrorEvent(data);
+                break;
+        }
+
     }
 
 }
